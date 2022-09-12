d70cc851b7170f354e1f7c25de058c32a0c301b9
LinOshiro <99516179+LinOshiro@users.noreply.github.com>
2022-09-12 13:30:21 -0500

    implemented the high score feature

--- script.js
+++ script.js
@@ -1,19 +1,8 @@
 'use strict';
-/*
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = '🎉 Correct Number!';
-
-console.log(document.querySelector('.message').textContent);
-
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
-
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
-*/
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -29,8 +18,12 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.number').textContent = secretNumber;
 
     document.querySelector('body').style.backgroundColor = '#60b347';
-
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
 
     // Guess is too high
   } else if (guess > secretNumber) {
@@ -59,16 +52,10 @@ document.querySelector('.check').addEventListener('click', function () {
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
-
   document.querySelector('.message').textContent = 'Start guessing...';
-
   document.querySelector('.score').textContent = score;
-
   document.querySelector('.number').textContent = '?';
-
   document.querySelector('.guess').value = '';
-
   document.querySelector('body').style.backgroundColor = '#222';
-
   document.querySelector('.number').style.width = '15rem';
 });
