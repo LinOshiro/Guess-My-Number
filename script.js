0842648b152b91fcf89f3dccd9095c16da5fe013
LinOshiro <99516179+LinOshiro@users.noreply.github.com>
2022-09-12 12:33:49 -0500

    Manipulating CSS style for winning game

--- script.js
+++ script.js
@@ -21,10 +21,19 @@ document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
+  // When there is no input
   if (!guess) {
     document.querySelector('.message').textContent = '⛔️ No Number!';
+
+    //When player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+
+    document.querySelector('.number').style.width = '30rem';
+
+    // Guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📈 Too High!';
@@ -34,6 +43,8 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = '💥 You Lost the game!';
       document.querySelector('.score').textContent = 0;
     }
+
+    // guess is too low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📉 Too Low!';
